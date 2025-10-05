43dabf05f70bb620e5dc0f5b28f81ac066ef2f46
EshaG-Solanki <gulyani.esha@gmail.com>
2025-10-06 10:16:25 +1100

    Added Portal View after deployment and cleaned up script

--- nsgwithESPandAHprotocols.tf
+++ nsgwithESPandAHprotocols.tf
@@ -1,7 +1,5 @@
  terraform {
- # required_version = ">= 1.9, < 2.0"
-
-  required_providers {
+ required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
@@ -19,10 +17,6 @@ provider "azurerm" {
   subscription_id = "71dbac3f-0c38-4127-833a-beb0f85925ad"
 }
 
-#module "naming" {
- # source  = "Azure/naming/azurerm"
-  #version = "0.3.0"
-#}
 
 # This is required for resource modules
 resource "azurerm_resource_group" "this" {
@@ -37,6 +31,7 @@ resource "azurerm_network_security_group" "example" {
           resource_group_name = azurerm_resource_group.this.name
         }
 
+#Esp port 500
 resource "azurerm_network_security_rule" "allow_Esp" {
           name                        = "Allow_Esp"
           priority                    = 101
@@ -51,7 +46,8 @@ resource "azurerm_network_security_rule" "allow_Esp" {
           resource_group_name         = azurerm_resource_group.this.name
         }
 
-        resource "azurerm_network_security_rule" "allow_ah" {
+# Ah port 4500
+resource "azurerm_network_security_rule" "allow_ah" {
           name                        = "Allow_ah"
           priority                    = 102
           direction                   = "Inbound"
